b5f152eb54672a93bc487ac004c1c3b3d2120b17
michaelsatumba <mjsatumba@gmail.com>
2022-02-11 19:14:10 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// alert('hello');
+
+function five() {
+	let x = document.getElementById('bill').value;
+	// alert(x);
+	result = x * 0.05;
+	let numberOfPeople = document.getElementById('people').value;
+	secondResult = result / numberOfPeople;
+	sum = x / numberOfPeople;
+	document.getElementById('tip').value = secondResult;
+	document.getElementById('total').value = sum;
+}
+
+function ten() {
+	let x = document.getElementById('bill').value;
+	// alert(x);
+	result = x * 0.1;
+	let numberOfPeople = document.getElementById('people').value;
+	secondResult = result / numberOfPeople;
+	sum = x / numberOfPeople;
+	document.getElementById('tip').value = secondResult;
+	document.getElementById('total').value = sum;
+}
+
+function fifteen() {
+	let x = document.getElementById('bill').value;
+	// alert(x);
+	result = x * 0.15;
+	let numberOfPeople = document.getElementById('people').value;
+	secondResult = result / numberOfPeople;
+	sum = x / numberOfPeople;
+	document.getElementById('tip').value = secondResult;
+	document.getElementById('total').value = sum;
+}
+
+function twentyFive() {
+	let x = document.getElementById('bill').value;
+	// alert(x);
+	result = x * 0.25;
+	let numberOfPeople = document.getElementById('people').value;
+	secondResult = result / numberOfPeople;
+	sum = x / numberOfPeople;
+	document.getElementById('tip').value = secondResult;
+	document.getElementById('total').value = sum;
+}
+
+function fifty() {
+	let x = document.getElementById('bill').value;
+	// alert(x);
+	result = x * 0.5;
+	let numberOfPeople = document.getElementById('people').value;
+	secondResult = result / numberOfPeople;
+	sum = x / numberOfPeople;
+	document.getElementById('tip').value = secondResult;
+	document.getElementById('total').value = sum;
+}
